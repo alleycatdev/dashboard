c83ebfcfd7c4655b80b2f17a5d87686ba8cd6c87
Silage Pete <silagepete@protonmail.com>
2020-10-21 00:07:06 -0700

    main @ 326b84620e11a9005965e87654f9f20d07ceb6a9

---
+++ precache-manifest.c47a2ab7d58743c99e5489f5ddf4661a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168dbadba32960cf952a06f36a1490a2",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "8b5f9652a8f7cf151ac7",
+    "url": "/dashboard/static/js/2.a252d1df.chunk.js"
+  },
+  {
+    "revision": "6988bff49f5cdadaf79a7f3e42ad113e",
+    "url": "/dashboard/static/js/2.a252d1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f5a57847e270a55150",
+    "url": "/dashboard/static/js/main.430b5653.chunk.js"
+  },
+  {
+    "revision": "6388236c331b6c879f55",
+    "url": "/dashboard/static/js/runtime-main.9f3afc0c.js"
+  },
+  {
+    "revision": "ca9d8aa8c3b8a90f77d1154a3f7250fb",
+    "url": "/dashboard/static/media/logo@3x.ca9d8aa8.png"
+  },
+  {
+    "revision": "0466c09f23546d695fd864881997a5cb",
+    "url": "/dashboard/static/media/png_eth@3x.0466c09f.png"
+  },
+  {
+    "revision": "d51efaf235064b217c8123ffb1eca8a7",
+    "url": "/dashboard/static/media/png_frenbtc_56px@3x.d51efaf2.png"
+  },
+  {
+    "revision": "658adb89db0e1ec097722e9711041538",
+    "url": "/dashboard/static/media/png_usdc_56px@3x.658adb89.png"
+  },
+  {
+    "revision": "2e5ffd23e81d51ee1677231e2773ef2b",
+    "url": "/dashboard/static/media/png_wbtc_56px@3x.2e5ffd23.png"
+  },
+  {
+    "revision": "4b49c39aae71e8a17c888dfcfe1ab49f",
+    "url": "/dashboard/static/media/wiki.4b49c39a.png"
+  }
+]);
